4f92da38613c186b29bd1c212d92a675ff685f40
May Shech <may745@gmail.com>
2023-03-12 16:27:05 +0200

    added models and routes to backend

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,12 @@ import express from "express";
 const app = express();
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-// import userRoute from "./routes/user.js";
-// import authRoute from "./routes/auth.js";
-// import productRoute from "./routes/product.js";
-// import cartRoute from "./routes/cart.js";
-// import orderRoute from "./routes/order.js";
-// import stripeRoute from "./routes/stripe.js";
+import userRoute from "./routes/user.js";
+import authRoute from "./routes/auth.js";
+import productRoute from "./routes/product.js";
+import cartRoute from "./routes/cart.js";
+import orderRoute from "./routes/order.js";
+import stripeRoute from "./routes/stripe.js";
 import cors from "cors";
 
 dotenv.config();
@@ -21,12 +21,12 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-// app.use("/api/auth", authRoute);
-// app.use("/api/users", userRoute);
-// app.use("/api/products", productRoute);
-// app.use("/api/carts", cartRoute);
-// app.use("/api/orders", orderRoute);
-// app.use("/api/checkout", stripeRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Backend server is running!\n" + "on port: " + process.env.PORT || 3000 + "\n");
