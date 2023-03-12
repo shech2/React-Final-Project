a8ea19eaa4a765c577a7520b026c981f5ae702f0
May Shech <may745@gmail.com>
2023-03-12 15:58:18 +0200

    working on backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,33 @@
-import express from 'express';
-
+import express from "express";
 const app = express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+// import userRoute from "./routes/user.js";
+// import authRoute from "./routes/auth.js";
+// import productRoute from "./routes/product.js";
+// import cartRoute from "./routes/cart.js";
+// import orderRoute from "./routes/order.js";
+// import stripeRoute from "./routes/stripe.js";
+import cors from "cors";
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+dotenv.config();
 
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DB Connection Successfull!"))
+    .catch((err) => {
+        console.log(err);
+    });
 
+app.use(cors());
+app.use(express.json());
+// app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/products", productRoute);
+// app.use("/api/carts", cartRoute);
+// app.use("/api/orders", orderRoute);
+// app.use("/api/checkout", stripeRoute);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Backend server is running!\n" + "on port: " + process.env.PORT || 3000 + "\n");
 });
