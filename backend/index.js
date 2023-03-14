c6f67a30d04cab94d591a35249a4337bac214c77
May Shech <may745@gmail.com>
2023-03-15 01:23:19 +0200

    added contorllers and fixed backend a bit

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,6 @@ import express from "express";
 const app = express();
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoute from "./routes/user.js";
-import authRoute from "./routes/auth.js";
 import productRoute from "./routes/product.js";
 import cartRoute from "./routes/cart.js";
 import orderRoute from "./routes/order.js";
@@ -21,8 +19,6 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
