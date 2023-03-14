2d9e58b0200d86d3308af45c4e20c8cc63b5f431
May Shech <may745@gmail.com>
2023-03-15 00:27:24 +0200

    fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,6 @@ import Homepage from './pages/Homepage';
 import Login from './pages/Login';
 import './App.css';
 //import ProductList from './pages/ProductList';
-<<<<<<< Updated upstream
-import Homepage from './pages/Homepage';
-
-
-const App = () => {
-  return <Homepage />;
-=======
 import Product from './pages/Product';
 import Register from './pages/Register';
 import Cart from './pages/Cart';
@@ -17,8 +10,7 @@ import Cart from './pages/Cart';
 
 
 const App = () => {
-  return <Homepage/>;
->>>>>>> Stashed changes
+  return <Homepage />;
 };
 
 
