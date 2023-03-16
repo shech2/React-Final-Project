1128097c79cb88eb762d0f04baf8f44ceaf64428
SemionSolonar <shimon22361@cs.colman.ac.il>
2023-03-16 12:38:52 +0200

    Add a sidebar for Admin page

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,14 @@
+import Sidebar from "./components/sidebar.js/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import "./app.css";
 
 function App() {
   return (
     <div>
       <Topbar />
-      
+      <div className="container">
+        <Sidebar />
+      </div>
     </div>
   );
 }
