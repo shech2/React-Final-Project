9ffe095e667a529cf17b3ff72a3e3b033c8141f6
SemionSolonar <shimon22361@cs.colman.ac.il>
2023-03-16 16:48:25 +0200

    fix bug

--- admin/src/pages/UserList/UserList.js
+++ admin/src/pages/UserList/UserList.js
@@ -2,17 +2,18 @@ import "./userList.css";
 import { DataGrid } from "@material-ui/data-grid";
 import { DeleteOutline } from "@material-ui/icons";
 import React from 'react'
+import { userRows } from "../../dummyData";
 
 export default function UserList() {
     const columns = [
         { field: 'id', headerName: 'ID', width: 90 },
         {
-            field: 'user', headerName: 'username', width: 200, renderCell: (params) => {
+            field: 'user', headerName: 'User', width: 200, renderCell: (params) => {
                 return (
                     <div className="userListUser">
                         <img className="userListImg " src={params.row.avatar}
                             alt="" />
-                        {params.row.Username}
+                        {params.row.username}
 
                     </div>
                 )
@@ -37,6 +38,7 @@ export default function UserList() {
             renderCell: (params) => {
                 return (
                     <>
+
                         <button className="userListEdit">Edit</button>
                         <DeleteOutline className="userListDelete" />
                     </>
@@ -45,53 +47,11 @@ export default function UserList() {
         },
     ];
 
-    const rows = [
-        {
-            id: 1, Username: 'jon Snow', avatar: "https://play-lh.googleusercontent.com/tmASL-0Jg5bq3RKsneEFVCcAth0M7jFLI7alQQyKSEqrpvLuMfW4mfkw4iSkLCj9_zo"
-            , email: "jon@gmail.com", status: "active", transaction: "$120.00"
-        },
-        {
-            id: 2, Username: 'jon Snow', avatar: "https://play-lh.googleusercontent.com/tmASL-0Jg5bq3RKsneEFVCcAth0M7jFLI7alQQyKSEqrpvLuMfW4mfkw4iSkLCj9_zo"
-            , email: "jon@gmail.com", status: "active", transaction: "$120.00"
-        },
-        {
-            id: 3, Username: 'jon Snow', avatar: "https://play-lh.googleusercontent.com/tmASL-0Jg5bq3RKsneEFVCcAth0M7jFLI7alQQyKSEqrpvLuMfW4mfkw4iSkLCj9_zo"
-            , email: "jon@gmail.com", status: "active", transaction: "$120.00"
-        },
-        {
-            id: 4, Username: 'jon Snow', avatar: "https://play-lh.googleusercontent.com/tmASL-0Jg5bq3RKsneEFVCcAth0M7jFLI7alQQyKSEqrpvLuMfW4mfkw4iSkLCj9_zo"
-            , email: "jon@gmail.com", status: "active", transaction: "$120.00"
-        },
-        {
-            id: 5, Username: 'jon Snow', avatar: "https://play-lh.googleusercontent.com/tmASL-0Jg5bq3RKsneEFVCcAth0M7jFLI7alQQyKSEqrpvLuMfW4mfkw4iSkLCj9_zo"
-            , email: "jon@gmail.com", status: "active", transaction: "$120.00"
-        },
-        {
-            id: 6, Username: 'jon Snow', avatar: "https://play-lh.googleusercontent.com/tmASL-0Jg5bq3RKsneEFVCcAth0M7jFLI7alQQyKSEqrpvLuMfW4mfkw4iSkLCj9_zo"
-            , email: "jon@gmail.com", status: "active", transaction: "$120.00"
-        },
-        {
-            id: 7, Username: 'jon Snow', avatar: "https://play-lh.googleusercontent.com/tmASL-0Jg5bq3RKsneEFVCcAth0M7jFLI7alQQyKSEqrpvLuMfW4mfkw4iSkLCj9_zo"
-            , email: "jon@gmail.com", status: "active", transaction: "$120.00"
-        },
-        {
-            id: 8, Username: 'jon Snow', avatar: "https://play-lh.googleusercontent.com/tmASL-0Jg5bq3RKsneEFVCcAth0M7jFLI7alQQyKSEqrpvLuMfW4mfkw4iSkLCj9_zo"
-            , email: "jon@gmail.com", status: "active", transaction: "$120.00"
-        },
-        {
-            id: 9, Username: 'jon Snow', avatar: "https://play-lh.googleusercontent.com/tmASL-0Jg5bq3RKsneEFVCcAth0M7jFLI7alQQyKSEqrpvLuMfW4mfkw4iSkLCj9_zo"
-            , email: "jon@gmail.com", status: "active", transaction: "$120.00"
-        },
-        {
-            id: 10, Username: 'jon Snow', avatar: "https://play-lh.googleusercontent.com/tmASL-0Jg5bq3RKsneEFVCcAth0M7jFLI7alQQyKSEqrpvLuMfW4mfkw4iSkLCj9_zo"
-            , email: "jon@gmail.com", status: "active", transaction: "$120.00"
-        },
-    ];
 
     return (
         <div className="userList">
             <DataGrid
-                rows={rows}
+                rows={userRows}
                 columns={columns}
                 pageSize={8}
                 checkboxSelection
