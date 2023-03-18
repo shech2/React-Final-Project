7bc7760c78a13b10f3565cd02118380b44602cd6
Yan Levit <yan2045@gmail.com>
2023-03-18 19:41:38 +0200

    fix to admin

--- admin/src/components/sidebar.js/Sidebar.js
+++ admin/src/components/sidebar.js/Sidebar.js
@@ -29,14 +29,6 @@ export default function Sidebar() {
                             Home
                         </li>
                         </Link>
-                        <li className="sidebarListItem">
-                            <Timeline className="sidebarIcon" />
-                            Analytics
-                        </li>
-                        <li className="sidebarListItem">
-                            <TrendingUp className="sidebarIcon" />
-                            Sales
-                        </li>
                     </ul>
                 </div>
                 <div className="sidebarMenu">
@@ -65,42 +57,7 @@ export default function Sidebar() {
                         </li>
                     </ul>
                 </div>
-                <div className="sidebarMenu">
-                    <h3 className="sidebarTitle">Notifications</h3>
-                    <ul className="sidebarList">
-                        <li className="sidebarListItem">
-                            <MailOutline className="sidebarIcon" />
-                            Mail
-                        </li>
-                        <li className="sidebarListItem">
-                            <DynamicFeed className="sidebarIcon" />
-                            Feedback
-                        </li>
-                        <li className="sidebarListItem">
-                            <ChatBubbleOutline className="sidebarIcon" />
-                            Messages
-                        </li>
-                    </ul>
                 </div>
-                <div className="sidebarMenu">
-                    <h3 className="sidebarTitle">Staff</h3>
-                    <ul className="sidebarList">
-                        <li className="sidebarListItem">
-                            <WorkOutline className="sidebarIcon" />
-                            Manage
-                        </li>
-                        <li className="sidebarListItem">
-                            <Timeline className="sidebarIcon" />
-                            Analytics
-                        </li>
-                        <li className="sidebarListItem">
-                            <Report className="sidebarIcon" />
-                            Reports
-                        </li>
-                    </ul>
                 </div>
-            </div>
-        </div >
-
     );
 }
