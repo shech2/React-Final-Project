7b2916f899c8342257d867686d3eea41d5bdbcb1
Yan Levit <yan2045@gmail.com>
2023-03-18 19:41:26 +0200

    fix to admin home

--- admin/src/pages/home/Home.js
+++ admin/src/pages/home/Home.js
@@ -6,11 +6,45 @@ import "./home.css"
 import { userData } from "../../dummyData"
 import { useEffect } from "react"
 import { userRequest } from "../../requestMethods"
+import { useState } from "react"
 
 export default function Home() {
-   
+    const [userStats, setUserStats] = useState([]);
+
+    const MONTHS = useMemo(
+      () => [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Agu",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      []
+    );
+  
+    useEffect(() => {
+      const getStats = async () => {
+        try {
+          const res = await userRequest.get("/users/stats");
+          res.data.map((item) =>
+            setUserStats((prev) => [
+              ...prev,
+              { name: MONTHS[item._id - 1], "Active User": item.total },
+            ])
+          );
+        } catch {}
+      };
+      getStats();
+    }, [MONTHS]);
+  
     return (
-    
     <div className="home">
             <Featuredinfo />
             <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
