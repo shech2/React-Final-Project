bc203aa79e71e6c56369fac0880f2461cae4cb34
May Shech <may745@gmail.com>
2023-03-18 23:56:16 +0200

    fix

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -19,6 +19,7 @@ export const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState();
 
     const signup = (email, password) => {
+        socket.emit('login', email);
         return createUserWithEmailAndPassword(Auth, email, password);
     }
 
